2109c59d4512a24d4c7615b84ebbc01e8aa64b96
Alessio Giovanelli <92802219+agiovanelli@users.noreply.github.com>
2026-04-10 08:12:27 +0200

    Update ParcheggioServiceImpl.java

--- codice/PMG-backend/src/main/java/pmg/backend/parcheggio/ParcheggioServiceImpl.java
+++ codice/PMG-backend/src/main/java/pmg/backend/parcheggio/ParcheggioServiceImpl.java
@@ -284,8 +284,8 @@ public class ParcheggioServiceImpl implements ParcheggioService {
         
         boolean disabile = "Si".equalsIgnoreCase(preferenzeUtente.get("disabile"));
         boolean incinta = "Si".equalsIgnoreCase(preferenzeUtente.get("donnaIncinta"));
-        int distanzaPreferita = parseIntOrDefault(preferenzeUtente.get("distanzaPreferita"), 1);
-
+        int distanzaPreferita = parseIntOrDefault(preferenzeUtente.get("distanza"), 1);
+        
         Posto migliorPosto = null;
         int punteggioMassimo = Integer.MIN_VALUE;
 
